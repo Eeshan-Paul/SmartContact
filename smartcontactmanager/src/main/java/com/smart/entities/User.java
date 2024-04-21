7f0eb676782d5ed3638112c23ec1c8dbe7a3762e
Eeshan Paul <eeshanatri2904@gmail.com>
2024-04-21 17:02:59 +0530

    Home page done

--- smartcontactmanager/src/main/java/com/smart/entities/User.java
+++ smartcontactmanager/src/main/java/com/smart/entities/User.java
@@ -21,6 +21,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int Id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	private String role;
